4efa49c2ec0df542722410f2c6c365b8d775efe0
Yves Chalant <ychalant@student.s19.be>
2024-08-06 18:22:00 +0200

    moved ft_transcendence folder to root to have a commong code base

--- src/ft_transcendence/ft_transcendence/urls.py
+++ src/ft_transcendence/ft_transcendence/urls.py
@@ -20,6 +20,5 @@ from django.urls import include, path
 #todo: instead of routing to urls, call views directly, the views make api calls to corresponding backend services.
 urlpatterns = [
 	path('', include("website.urls")),
-    path('admin/', admin.site.urls),
-	path('auth/', include("authentication.urls"))
+    path('admin/', admin.site.urls)
 ]
--- src/ft_transcendence/website/urls.py
+++ src/ft_transcendence/website/urls.py
@@ -4,7 +4,9 @@ from django.urls import include, path
 from . import views
 from .views import ItemListView
 
+#todo: for each path, call the view code
 urlpatterns = [
 	path('', views.index, name="index"),
-	path('items/', ItemListView.as_view(), name='item-list')
+	path('items/', ItemListView.as_view(), name='item-list'),
+	path('auth/', include("authentication.urls"))
 ]
