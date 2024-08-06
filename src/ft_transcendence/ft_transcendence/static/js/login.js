78050ef0f8eff7dc61387f9527cb6da87d9ad9cb
Yves Chalant <ychalant@student.s19.be>
2024-08-06 17:57:32 +0200

    microservices preparations and refactoring

---
+++ src/ft_transcendence/ft_transcendence/static/js/login.js
@@ -0,0 +1,47 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	var csrftoken = getCookie('csrftoken');
+
+	var	loginForm = document.getElementById("login-form");
+	var	loginUserName = document.getElementById("login-username");
+	var	loginPassword = document.getElementById("login-password");
+
+	loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+		//todo: compare login confirm with password.
+        const credentials = {
+            username: loginUserName.value,
+            password: loginPassword.value
+        };
+        fetch('/auth/login/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+				'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(response => response.json())
+		//todo: confirm login.
+        // .then(data => {
+        //     fetchItems(); // Refresh the item list
+        //     itemForm.reset(); // Clear the form
+        // });
+    })
+});
---
+++ src/ft_transcendence/ft_transcendence/static/js/signup.js
@@ -0,0 +1,48 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	var csrftoken = getCookie('csrftoken');
+
+	var	signupForm = document.getElementById("signup-form");
+	var	signupUserName = document.getElementById("signup-username");
+	var	signupPassword = document.getElementById("signup-password");
+	var	signupConfirmPassword = document.getElementById("signup-confirm-password");
+
+	signupForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+		//todo: compare signup confirm with password.
+        const credentials = {
+            'username': signupUserName.value,
+            'password': signupPassword.value
+        };
+        fetch('/auth/signup/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+				'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(response => response.json())
+		//todo: confirm signup.
+        // .then(data => {
+        //     fetchItems(); // Refresh the item list
+        //     itemForm.reset(); // Clear the form
+        // });
+    })
+});
