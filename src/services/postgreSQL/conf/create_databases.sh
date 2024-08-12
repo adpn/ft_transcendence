82d0786f174ae16fdcdf52f31e9cac5615b97b34
adupin <adupin@student.s19.be>
2024-08-12 16:04:10 +0200

    Creating DBs in postgresql, successfully implemented in website-authentification

---
+++ src/services/postgreSQL/conf/create_databases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Wait for PostgreSQL to start
+until psql -U "$POSTGRES_USER" -c '\q' > /dev/null 2>&1; do
+    echo "Waiting for PostgreSQL to be available..."
+    sleep 2
+done
+
+# Create databases
+createdb -U "$POSTGRES_USER" user_data
+createdb -U "$POSTGRES_USER" game_history
+createdb -U "$POSTGRES_USER" authentication
--- src/services/website/src/src/settings.py
+++ src/services/website/src/src/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'authentication',
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
 
