af8797b84ad422e9049f6fed1640c467df5d3c67
adupin <adupin@student.s19.be>
2024-08-19 15:56:04 +0200

    set up nginx

---
+++ src/services/proxy-server/tools/run_service.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+MAX_RETRIES=60
+
+# retries=0
+
+# echo "Waiting for authentication service to be ready..."
+# while ! nc -z user-management 8000; do
+# 	retries=$((retries + 1))
+# 	sleep 1
+# 	if [ $retries -ge $MAX_RETRIES ]; then
+# 		echo "Reached maximum number of retries, exiting..."
+# 		exit 1
+# 	fi
+# done
+
+retries=0
+
+echo "Waiting for website service to be ready..."
+while ! nc -z website 8000; do
+	retries=$((retries + 1))
+	sleep 1
+	if [ $retries -ge $MAX_RETRIES ]; then
+		echo "Reached maximum number of retries, exiting..."
+		exit 1
+	fi
+done
+
+nginx -g daemon off
