7cd3a3f6f0e363e81a61d9bbb2b3a7a39913aa46
adupin <adupin@student.s19.be>
2024-09-20 14:33:37 +0200

    new stats and better looking in user profile

--- src/services/users/src/user_data/online.py
+++ src/services/users/src/user_data/online.py
@@ -1,7 +1,8 @@
 import os
 import django
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import sync_to_async
+# from asgiref.sync import sync_to_async
 # from authentication.models import User
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
@@ -9,30 +10,25 @@ django.setup()
 
 from user_data.models import UserProfile
 
+@database_sync_to_async
+def set_player_online_status(user, status : bool):
+    profile = UserProfile.objects.get(user=user)
+    profile.online_status = status
+    profile.save()
+
 class OnlineStatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         print(f"WebSocket connection attempt from {self.scope['client']}", flush=True)
         if self.scope["user"].is_authenticated:
             self.user = self.scope["user"]
-            print("Connecting user: ", self.user.username, flush=True)
-            await self.mark_online(self.user)
+            print("Connecting user: ", self.user.id, flush=True)
+            await set_player_online_status(self.user, True)
             await self.accept()
         else:
             await self.close()
 
     async def disconnect(self, close_code):
         if self.scope["user"].is_authenticated:
-            print("Disconnecting user: ", self.user.username, flush=True)
-            await self.mark_offline(self.scope["user"])
-
-    @staticmethod
-    async def mark_online(user):
-        profile = await sync_to_async(UserProfile.objects.get)(user=user)
-        profile.online_status = True
-        await sync_to_async(profile.save)()
-
-    @staticmethod
-    async def mark_offline(user):
-        profile = await sync_to_async(UserProfile.objects.get)(user=user)
-        profile.online_status = False
-        await sync_to_async(profile.save)()
+            print("Disconnecting user: ", self.user.id, flush=True)
+            print("Close code: ", close_code, flush=True)
+            await set_player_online_status(self.user, False)
